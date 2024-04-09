8935f0a65beba3fd2113bc45bfae0b78ac29868a
Reph <sel@navi.home>
2024-04-09 16:25:37 +0200

    just auth

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -18,6 +18,10 @@ class RegistrationController extends AbstractController
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
@@ -25,7 +29,6 @@ class RegistrationController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             // add timestamp at registration
             $user->setCreatedAt(new \DateTimeImmutable());
-
             // set default avatar
             $user->setAvatar("blank url");
             // encode the plain password
