9b435922c7b5a349bd6894fbd39f93d51c26e54f
reph-web <s3lonline@proton.me>
2024-07-24 17:19:27 +0200

    login error displayed

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,18 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+use App\Entity\Group;
+use App\Entity\Transaction;
+use App\Entity\Notification;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use DateTimeImmutable;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,9 +8,18 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(
+    fields: 'email', 
+    message: 'This email already used, please log in'
+)]
+#[UniqueEntity(
+        fields: 'username', 
+        message: 'This username already used'
+)]
+    
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
