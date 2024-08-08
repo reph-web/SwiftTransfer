dbde57650d1b6d1ec3c9603aace9d255d4b7c476
reph-web <s3lonline@proton.me>
2024-08-08 15:32:16 +0200

    worked on the frontend of homepage and profile page

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,8 +15,21 @@ class HomePageController extends AbstractController
         if(!$this->getUser()){
             return $this->redirectToRoute('app_login');
         }
+        /**
+         * @var User
+         */
+        $user = $this->getUser();
+
+        $transactionsSendedArray = $user->getTransactionSended()->toArray();
+        $transactionsReceivedArray = $user->getTransactionsReceived()->toArray();
+        $mergedTransactions = array_merge($transactionsSendedArray, $transactionsReceivedArray);
+        usort($mergedTransactions, function ($a, $b) {
+            return $a->getCreatedAt() <=> $b->getCreatedAt();
+        });
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'transactions' => $mergedTransactions,
         ]);
     }
 }
